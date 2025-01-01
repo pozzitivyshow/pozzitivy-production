9fc71c3fed1c9149e970138fe929cf1d18c540d3
pozzitivy-show-tech <enpozzitivy@gmail.com>
2025-01-01 12:53:51 +0100

    updated series interface

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let soundControlButtonMute = document.querySelector(".sound-control-button-mute"
 let fullscreenButton = document.querySelector(".fullscreen-button");
 let playedLength = document.querySelector(".played-length");
 let totalLength = document.querySelector(".total-length");
+let episodesPopup = document.querySelector(".episodes-popup");
 
 let timerID;
 
@@ -141,6 +142,34 @@ function previewPlayback(event){
     playedLength.style.width = previewLength;
 }
 
+function closePanel(){
+    episodesPopup.style.opacity = "0";
+    episodesPopup.style.zIndex = "-1";
+}
+function openPanel(){
+    episodesPopup.style.zIndex = "10";
+    episodesPopup.style.opacity = "100%";
+}
+function headerBlur(){
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        document.querySelector(".header").style.backdropFilter = "blur(10px) brightness(70%)";
+    }
+    else{
+        document.querySelector(".header").style.backdropFilter = "none";
+    }
+}
+window.onscroll = function() {headerBlur()};
+document.querySelector(".header").onmouseenter = function(){
+    document.querySelector(".header").style.backdropFilter = "blur(10px) brightness(70%)";
+}
+document.querySelector(".header").onmouseleave = function(){
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+
+    }
+    else{
+        document.querySelector(".header").style.backdropFilter = "none";
+    }
+}
 videoControls.addEventListener("click", videoPausePlay);
 playButtonPlayback.addEventListener("click", videoPausePlay);
 pauseButtonPlayback.addEventListener("click", videoPausePlay);
